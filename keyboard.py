b4337861e30792abf43950d088e5b45244f4cdd9
Veronika Kukharskaya <verka04052001@gmail.com>
2023-04-10 20:31:26 +0300

    Добавлены колбэк и ин-лайн кнопки

--- keyboard.py
+++ keyboard.py
@@ -11,3 +11,21 @@ show_user = types.KeyboardButton(text="Покажи пользователя")
 send_photo = types.KeyboardButton(text="Отправить картинку")
 
 start.add(stats, info, develop, show_user, send_photo)
+
+"""Ин-лайн кнопки"""
+# for /links: 2 button inline
+urlkb = InlineKeyboardMarkup(row_width=1)
+inline_btn_1 = InlineKeyboardButton(text='Документация по Python', url='https://docs.python.org/3/index.html')
+inline_btn_2 = InlineKeyboardButton(text='Курсы по Python',
+                                    url='https://myitschool.by/kursy-it/razrabotka-veb-prilozhenij-na-python/')
+urlkb.add(inline_btn_1, inline_btn_2)
+
+# 2 button for statistic(колбэк кнопки)
+statis = InlineKeyboardMarkup()
+statis.add(InlineKeyboardButton('Да', callback_data='join'))  # создаем кнопку и кэлбэк к ней
+statis.add(InlineKeyboardButton('Нет', callback_data='cancel'))  # создаем кнопку и кэлбэк к ней
+
+# 2 button for Покажи пользователя(колбэк кнопки)
+show_us = InlineKeyboardMarkup()
+show_us.add(InlineKeyboardButton('Хочу видеть id', callback_data='show'))
+show_us.add(InlineKeyboardButton('Вернуться обратно', callback_data='exit'))
